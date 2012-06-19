2bcb3bd9041f179c367cc5d288ffd1741aa410b6
Boris Shemigon <i@boris.co>
2012-06-20 01:00:29 +0700

    ample usage of the api
    Params:
    	file - it saves README into the database
    	url  - it tries to obtain README's url
    
    The output will be like if succeeded
    README = files/9da1f8e0aecc9d868bad115129706a77/4fe0bc175a51b83eee000000/"
    and if failed
    README =

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import mimetypes
 from bson.objectid import ObjectId
 from flask import Flask, request, abort
 from pymongo import Connection
@@ -16,7 +17,6 @@ USER_FILE_URLS = {
 USER_ACCESS_KEYS = {
     'test_user': '9da1f8e0aecc9d868bad115129706a77'
 }
-
 
 app = Flask(__name__)
 
@@ -40,9 +40,33 @@ def user_by_key(key):
     return None
 
 
+def current_user():
+    '''
+    In real application should be changed to something meaningful
+    '''
+    return 'test_user'
+
+
 def url_for(user, oid):
-    pattern = USER_FILE_URLS.get(user, BASE_FILE_URL)
-    return pattern % {'user': user, 'oid': oid}
+    if storage(user).exists(ObjectId(oid)):
+        pattern = USER_FILE_URLS.get(user, BASE_FILE_URL)
+        return pattern % {'user': USER_ACCESS_KEYS[user], 'oid': oid}
+
+
+def save_file(user, file):
+    if file:
+        content = file.read()
+        fs = storage(user)
+        with fs.new_file(filename=check_filename(file.filename),
+                         content_type=file.content_type) as f:
+            f.write(content)
+        return unicode(f._id)
+
+
+def oid_by_name(user, filename):
+    fs = storage(user)
+    if fs.exists(filename=filename):
+        return unicode(fs.get_last_version(filename=filename)._id)
 
 
 @app.route('/file/<key>/')
@@ -66,14 +90,7 @@ def upload_file(key):
     user = user_by_key(key)
     if not user:
         abort(404)
-    file = request.files['file']
-    if file:
-        content = file.read()
-        fs = storage(user)
-        with fs.new_file(filename=check_filename(file.filename),
-                         content_type=file.content_type) as f:
-            f.write(content)
-        return unicode(f._id)
+    return save_file(user, request.files['file'])
 
 
 @app.route('/file/<key>/<filename>/')
@@ -81,9 +98,9 @@ def get_id_by_filename(key, filename):
     user = user_by_key(key)
     if not user:
         abort(404)
-    fs = storage(user)
-    if fs.exists(filename=filename):
-        return unicode(fs.get_last_version(filename=filename)._id)
+    oid = oid_by_name(user, filename)
+    if oid:
+        return oid
     abort(404)
 
 
@@ -92,11 +109,56 @@ def link_to_file(key, oid):
     user = user_by_key(key)
     if not user:
         abort(404)
-    fs = storage(user)
-    if fs.exists(ObjectId(oid)):
-        return url_for(user, oid)
+    url = url_for(user, oid)
+    if url:
+        return url
     abort(404)
 
 
+class File(object):
+    def __init__(self, filename, content_type=None):
+        self.fileHandle = open(filename, 'rb')
+        self.filename = filename
+        self.content_type = (content_type
+                             or mimetypes.guess_type(filename)[0]
+                             or 'application/octet-stream')
+
+    def read(self):
+        return self.fileHandle.read()
+
+
+def sample_save_from_filesystem(filename):
+    user = current_user()
+    if user:
+        try:
+            fileHandle = File(filename)
+            oid = save_file(user, fileHandle)
+            return url_for(user, oid)
+        except IOError:
+            pass
+    return ''
+
+
+def sample_get_file_url(filename):
+    user = current_user()
+    if user:
+        oid = oid_by_name(user, filename)
+        if oid:
+            return url_for(user, oid)
+    return ''
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    import sys
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'save':
+            filename = 'README'
+            url = sample_save_from_filesystem(filename)
+            print(u'%s = %s' % (filename, url))
+        elif sys.argv[1] == 'url':
+            filename = 'README'
+            url = sample_get_file_url(filename)
+            print(u'%s = %s' % (filename, url))
+    else:
+        app.run(host='0.0.0.0', port=5000, debug=True)
